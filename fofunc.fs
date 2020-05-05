cb5f1120e51894bb64a6e1ae9d55cf1b1010856a
Magnus <magnujo@ruc.dk>
2020-05-05 17:27:09 +0200

    2 exp

--- fofunc.fs
+++ fofunc.fs
@@ -1,22 +1,24 @@
-module Fofunc
-open Parser 
-open VM
+//module Fofunc
+//open Parser 
+//open VM
 
-let Parser = #r "parser.dll"
+//let Parser = #r "parser.dll"
 
 // AST of a language with functions
+
 
 type varname  = string
 type funcname = string
 type exp      = | INT     of int             // i
                 | ADD     of exp * exp       // e1 + e2
                 | VAR     of varname         // x
-                | CALL    of funcname * exp  // f ( e )                
-type func     = funcname * (varname * exp)   // func f ( x ) = e
+                | CALL    of funcname * exp list  // f ( e )                
+type func     = funcname * (varname list * exp)   // func f ( x ) = e
+
 
 // Interpreter
 
-type 'a env = (varname * 'a) list
+(* type 'a env = (varname * 'a) list
 let rec lookup x = function
   | []            -> failwith ("unbound: " + x)
   | (y, w) :: env -> if x = y then w else lookup x env
@@ -31,17 +33,9 @@ let evalProg (funcs, e) =
                          eval [(x, v)] body              // evaluate with a small environment only containing the "x" bound to v which fx evaluates to 8 in the below example. With this env it evaluates the body which is fx ADD(VAR "x", INT 42). This evaluation will lookup the "x" in the env and find the value 8 and add it to 42.
   eval [] e                                              // ??
 
-evalProg ([("foo", ("x", ADD (VAR "x", INT 42)))], CALL("foo", ADD(INT 4, INT 4))) 
+evalProg ([("foo", ("x", ADD (VAR "x", INT 42)))], CALL("foo", ADD(INT 4, INT 4)))  *)
 
 
-
-type varname  = string
-type funcname = string
-type exp      = | INT     of int             // i
-                | ADD     of exp * exp       // e1 + e2
-                | VAR     of varname         // x
-                | CALL    of funcname * exp list  // f ( e )                
-type func     = funcname * (varname * exp list)   // func f ( x ) = e
 
 // Interpreter
 
@@ -64,7 +58,7 @@ let evalProg (funcs, e) =
                             eval [(x, v1); (k, v2)] body                       
   eval [] e                                              
 
-//evalProg ([("foo", (["x"], ADD (VAR "x", INT 6)))], CALL("foo", [INT 6]))
+//evalProg ([("foo", (["x"; "y"], ADD (VAR "x", VAR "y")))], CALL("foo", [INT 6; INT 5])) // <-- works
 
 // foo(x, y) = x + y 
 // foo(4, 8) = 4 + 8
@@ -92,19 +86,21 @@ let rec private find l = function
   | (ILAB l' :: ins) -> if l = l' then ins else find l ins
   | (_       :: ins) -> find l ins
 
+// [IPUSH 8; IPUSH 42; ICALL 0; ILAB 1; I SWAP; IPOP; IHALT; ILAB 0; IGET 1; IGET 3; IADD; ISWAP; IRETN]
+
 let execProg prog st =
   let rec exec ins st =
     match (ins, st) with
       | ([],                v :: _)       -> v
-      | (IHALT     :: _,    v :: _)       -> v
-      | (IPUSH i   :: ins,  st)           -> exec ins (i :: st)
-      | (IGET p    :: ins,  st)           -> exec ins (get p st :: st)
-      | (IADD      :: ins,  y :: x :: st) -> exec ins (x + y :: st)
-      | (IPOP      :: ins,  _ :: st)      -> exec ins st
-      | (ISWAP     :: ins,  y :: x :: st) -> exec ins (x :: y :: st)
-      | (ICALL p   :: ILAB l :: _, st)    -> exec (find p prog) (l :: st)
+      | (IHALT     :: _,    v :: _)       -> v                             
+      | (IPUSH i   :: ins,  st)           -> exec ins (i :: st)       //8   // 42; 8     
+      | (IGET p    :: ins,  st)           -> exec ins (get p st :: st)      // 42; 1; 42; 8  //8; 42; 1; 42; 8
+      | (IADD      :: ins,  y :: x :: st) -> exec ins (x + y :: st)         // 50; 1; 42; 8
+      | (IPOP      :: ins,  _ :: st)      -> exec ins st                    // 50; 8
+      | (ISWAP     :: ins,  y :: x :: st) -> exec ins (x :: y :: st)        // 1; 50; 42; 8   //42; 50; 8
+      | (ICALL p   :: ILAB l :: _, st)    -> exec (find p prog) (l :: st)   // 1; 42; 8
       | (ILAB  l   :: ins,  st)           -> exec ins st
-      | (IRETN     :: _,    l :: st)      -> exec (find l prog) st
+      | (IRETN     :: _,    l :: st)      -> exec (find l prog) st          //50; 42; 8
   exec prog st
 
 // Compiler
@@ -121,39 +117,64 @@ let rec varpos x = function
 
 
 
-let rec comp fenv env = function // compiles expressions
+let rec comp fenv env = function                       // compiles function arg and function calc
   | INT i               -> [IPUSH i]
-  | ADD (e1, e2)        -> comp fenv env         e1 @  //comp fenv ["", "x"] VAR "X" ->  IGET 1
-                           comp fenv ("" :: env) e2 @  //comp fenv ["", "", "x"] INT 42  -> IPUSH 42
+  | ADD (e1, e2)        -> comp fenv env         e1 @  // comp fenv ["", "x"] VAR "X" ->  IGET 1
+                           comp fenv ("" :: env) e2 @  // comp fenv ""::""::"x"::[] VAR "Y" ->
                            [IADD]                      // [IGET1, IPUSH 42, IADD]
   | VAR x               -> [IGET (varpos x env)]
-  | CALL (f, [a1])      -> let lr = newLabel()    //lr = 1 
-                           let lf = lookup f fenv // lf = 0
-                           comp fenv env a1  @     // comp fenv [] INT 8 -> [IPUSH 8]
-                           [ICALL lf]       @     // [ICALL 0]
-                           [ILAB lr]        @     // [ILAB 1]                       
-                           [ISWAP]          @     // [ISAWP]
-                           [IPOP]                 // [IPOP]                         [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP]
-
+  | CALL (f, [a1])      -> let lr = newLabel()         // lr = 1 
+                           let lf = lookup f fenv      // lf = 0
+                           comp fenv env a1  @         // comp fenv [] INT 8 -> [IPUSH 8]
+                           [ICALL lf]        @         // [ICALL 0]
+                           [ILAB lr]         @         // [ILAB 1]                       
+                           [ISWAP]           @         // [ISAWP]
+                           [IPOP]                      // [IPOP] -> [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP]
+  | CALL (f, [a1; a2])  -> let lr = newLabel()         // lr = 1 
+                           let lf = lookup f fenv      // lf = 0
+                           comp fenv env a1 @          //[IPUSH 8]  
+                           comp fenv env a2 @          //[IPUSH 8; IPUSH 42]
+                           [ICALL lf]       @          //[ICALL 0]  
+                           [ILAB lr]        @          //[ILAB 1]                        
+                           [ISWAP]          @          
+                           [IPOP]           @
+                           [ISWAP]          @             
+                           [IPOP]                   //[IPUSH 8; IPUSH 42; ICALL 0; ILAB 1; I]   
   
 
 let compProg (funcs, e1) = // compiles functions
   let fenv = List.map (fun (f, _) -> (f, newLabel())) funcs
   let rec compFuncs = function
-    | []                      -> comp fenv [] e1 @        // compiles the argument that is passed into the function, which also calles the function body with ICALL 
-                                 [IHALT]                  // [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT]  
-    | (f, (([x], a1)) :: funcs -> let lf = lookup f fenv   //lf = 0
-                                 compFuncs funcs     @    //[IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT]
-                                 [ILAB lf]           @    //[IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT, ILAB 0]
-                                 comp fenv [""; x] a1 @   //[IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT, ILAB 0, IGET1, IPUSH 42, IADD]  - compiles instructions for the body of the function (the calculations)
-                                 [ISWAP]             @    //[IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT ILAB 0, IGET1, IPUSH 42, IADD, ISWAP]
-                                 [IRETN]                  //[IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT, ILAB 0, IGET 1, IPUSH 42, IADD, ISWAP, IRETN]
-  compFuncs funcs                                         //[IPUSH 8; ICALL 0; ILAB 1; ISWAP; IPOP; IHALT; ILAB 0; IGET 1; IPUSH 42; IADD; ISWAP; IRETN]
+    | []                           -> comp fenv [] e1 @             // compiles the argument that is passed into the function, which also calls the function body with ICALL 
+                                      [IHALT]                       // [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT] <- list with argument on top of list. swap and pops to remove the return call?
+    | (f, ([x], e)) :: funcs       -> let lf = lookup f fenv        // lf = 0
+                                      compFuncs funcs     @         // [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT]
+                                      [ILAB lf]           @         // [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT, ILAB 0]
+                                      comp fenv [""; x] e @         // [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT, ILAB 0, IGET1, IPUSH 42, IADD]  - compiles instructions for the body of the function (the calculations)
+                                      [ISWAP]             @         // [IPUSH 8, ICALL 0, ILAB 1, ISWAP, IPOP, IHALT ILAB 0, IGET1, IPUSH 42, IADD, ISWAP]
+                                      [IRETN]  
+    | (f, ([x1; x2], e)) :: funcs  -> let lf = lookup f fenv   
+                                      compFuncs funcs          @    //[IPUSH 8; IPUSH 42; ICALL 0; ILAB 1; I SWAP; IPOP; IHALT;
+                                      [ILAB lf]                @    //[ILAB 0]
+                                      comp fenv [""; x1; x2] e @    //
+                                      [ISWAP]                  @    
+                                      [IRETN]                       //[IGET 1; IGET 3; IADD; ISWAP; IRETN]
+  compFuncs funcs                                          
 
 
+//[IPUSH 8; IPUSH 42; ICALL; ILAB 1; I SWAP; IPOP; IHALT; ILAB 0; IGET 1; IGET 3; IADD; ISWAP; IRETN]
 
-compProg ([("foo", ("x", [ADD (VAR "x", INT 42)]))], CALL("foo", [INT 8])) 
+compProg ([("foo", (["x"], ADD (VAR "x", INT 42)))], CALL("foo", [INT 8]))          //<- works
+let exL = compProg ([("foo", (["x"; "y"], ADD (VAR "x", VAR "y")))], CALL("foo", [INT 10; INT 42]))          
 
+execProg exL []
+
+// It compiles the expression (e) into a push of the function argument (in this case PUSH 8). 
+// Appends a call inst to the function calculation, aswell as a return label, to return to 
+// when the function calculation is done (in this case ICALL 0 + ILAB 1). The swap and pop is appended 
+// to get rid of the function argument. Appends the ILAB for the function body calculation 
+// and appends instrunctions for the functions body after that ILAB. After the body is executed, 
+// the value of the body is on top of the stack, and the return label is on position 1, therefore the ISWAP. 
 
 (* 
 let rec comp fenv env = function
@@ -188,10 +209,6 @@ let compProg (funcs, e1) =
 
 compProg ([("foo", ("x", [ADD (VAR "x", INT 42)]))], CALL("foo", [INT 8]))  *)
 
-// It compiles the expression (e) into a push of the function argument (in this case PUSH 8). Appends a call inst to the function calculation, 
-// aswell as a return label, to return to when the function calculation is done (in this case ICALL 0). The swap and pop i appended to get rid of the function argument. 
-// Appends the ILAB for the function body calculation and appends instrunctions for the functions body after that ILAB. After the body is executed, the value of the body is 
-//on top of the stack, and the return label is on position 1, therefore the ISWAP. 
 
 
 (* 
